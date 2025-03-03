671a0957c3161b0e1f749817ef51a566e6a6401b
Jack Myers <jmyers@jackmyers.one>
2025-03-03 22:42:51 +0800

    fix: correct value of Vector2 NEG_X and NEG_Y constants

--- src/main/java/one/jackmyers/icwatask/model/Vector2.java
+++ src/main/java/one/jackmyers/icwatask/model/Vector2.java
@@ -8,8 +8,8 @@ public record Vector2(int x, int y) {
   public static final Vector2 MAX = new Vector2(Integer.MAX_VALUE, Integer.MAX_VALUE);
   public static final Vector2 X = new Vector2(1, 0);
   public static final Vector2 Y = new Vector2(0, 1);
-  public static final Vector2 NEG_X = new Vector2(1, 0);
-  public static final Vector2 NEG_Y = new Vector2(0, 1);
+  public static final Vector2 NEG_X = new Vector2(-1, 0);
+  public static final Vector2 NEG_Y = new Vector2(0, -1);
 
   public Vector2 add(Vector2 other) {
     return new Vector2(this.x + other.x, this.y + other.y);
